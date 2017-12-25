c3e86101fffb66ed1539e390cb5787b5df13a133
alony <alony@wix.com>
2017-12-25 13:13:41 +0200

    day-24 py

--- 2017/day-24.py
+++ 2017/day-24.py
@@ -1,29 +1,24 @@
-input = [x for x in open("input-24","r").read().strip().split("\n")]
-
-def get_bridges2(prev, ports):
+def get_bridges(prev, ports):
     lastPort = prev[-1]
     candidates = [port for port in ports if lastPort[1] == port[0] or lastPort[1] == port[1]]
+    bridges = []
     if not candidates:
-        return [prev]
+        bridges.append(prev)
     else:
-        res = []
         for port in candidates:
             portIndex = ports.index(port)
             nextPorts = [x for x in ports if x != port]
             nextPrev = [x for x in prev]
             nextPrev.append(port if lastPort[1] == port[    0] else [port[1], port[0]])
-            nextBridges = get_bridges2(nextPrev, nextPorts)
+            nextBridges = get_bridges(nextPrev, nextPorts)
             for bridge in nextBridges:
-                res.append(bridge)
-        return res
+                bridges.append(bridge)
+    return bridges
 
-input = open("input-24", "r").read().strip().split("\n")
-ports = []
-for row in [x for x in input]:
-    a, b = [int(x) for x in row.split('/')]            
-    ports.append([int(a), int(b)])
+input = open("day-24.input", "r").read().strip().split("\n")
+ports = list(map(lambda x: [int(y) for y in x.split('/')], input))
+bridges = get_bridges([[0,0]], ports)
 
-bridges = get_bridges2([[0,0]], ports)
 maxVal = 0
 maxBridge = []
 for bridge in bridges:
@@ -34,10 +29,3 @@ for bridge in bridges:
         maxVal = sumVal
         maxBridge = bridge
 print(maxVal)
-print(max)
-#for bridge in get_bridges([(0,0)], ports):
-#        bridges.append((len(bridge), sum(a+b for a,b in bridge)))
-#part1 = sorted(bridges, key=lambda x: x[1])[-1][1]
-#print('part1 %s' % part1)
-# part2 = sorted(solution)[-1][1]
-# print('part2 %s' % part2)
