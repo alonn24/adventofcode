29e0774bbeb2e4bc0abd33aa485129d1738a7374
Alon Yehezkel <alonyehezkel@Alons-MacBook-Pro.local>
2022-12-25 19:39:29 +0200

    day 3

--- 2022/js/day3.js
+++ 2022/js/day3.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
+const { chunk } = require('lodash/fp');
 const input = fs.readFileSync('2022/input/day-3.input.txt').toString();
-
 
 const rucksacks = input.split('\n').map(row => [row.slice(0, row.length/2), row.slice(row.length/2, row.length)])
 const matching = rucksacks.map(rucksack => {
@@ -20,5 +20,12 @@ console.log(matching.reduce((res, x) => {
 	return res + getPriority(x);
 }, 0))
 
-
-
+const groups = chunk(3, input.split('\n'));
+const commonItemTypes = groups.map(([a,b,c]) => {
+	const aMap = new Set(a);
+	const bMap = new Set(b);
+	return c.split('').find(x => aMap.has(x) && bMap.has(x));
+});
+console.log(commonItemTypes.reduce((res, x) => {
+	return res + getPriority(x);
+}, 0))
