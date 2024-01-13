4f795a649811858b07eb1e3f0e6805c8d18622a8
Alon Yehezkel <alonyehezkel@Alons-MacBook-Pro-2.local>
2024-01-13 11:18:35 +0200

    2023 day 1

--- 2023/python/day-1.py
+++ 2023/python/day-1.py
@@ -12,20 +12,21 @@ def get_sum_of_digits():
 
 print(f'part 1 - {get_sum_of_digits()}')  # 53334
 
-words = ['one', 'two', 'three', 'four',
+words = ['zero', 'one', 'two', 'three', 'four',
          'five', 'six', 'seven', 'eight', 'nine']
 words_regex = f'([0-9]|{"|".join(words)})'
 
 
 def convert_to_digit(word):
     if word.isdigit():
-        return word
-    return f'{words.index(word) + 1}'
+        return int(word)
+    return words.index(word)
 
 
 def get_sum_of_all_digits():
     digits = map(lambda x: re.findall(words_regex, x), input)
-    numbers = map(lambda x: int(f'{convert_to_digit(x[0])}{convert_to_digit(x[-1])}'), digits)
+    numbers = map(lambda x: convert_to_digit(
+        x[0])*10+convert_to_digit(x[-1]), digits)
     return reduce(lambda a, b: a + b, numbers)
 
 
