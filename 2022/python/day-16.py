0cce6804c8907f24e8d480c0eacb3687eddd206f
Alony <alonn24@gmail.com>
2024-01-15 12:57:44 +0200

    slow recursive version

--- 2022/python/day-16.py
+++ 2022/python/day-16.py
@@ -1,5 +1,6 @@
 from functools import reduce
 import re
+from functools import wraps
 
 input = open("2022/input/day-16.input.txt", "r")
 
@@ -12,37 +13,37 @@ def extract_values_from_row(row):
     return result
 
 
-def traverse_valves(valve, all_valves, opened_valves, minutes_left):
+all_valves = reduce(lambda result, row: result | extract_values_from_row(row),
+                    open("2022/input/day-16.input.txt"),
+                    {})
 
+
+def traverse_valves(valve, opened_valves, minutes_left):
     if (minutes_left <= 0):
         return 0
     # count the current valve by opened
-    # current_round_sum = sum(
-    #     map(lambda x: all_valves[x]['rate'] * minutes_left, opened_valves))
     related_valves = all_valves[valve]['related_valves']
 
     # decide to opened the valve - we decrease 2 minutes, 1 extra for opening the valve
     open_valve_sums = [0]
-    current_round_sum = 0 # if we open the valve, sum up the valve pressure
     if all_valves[valve]['rate'] > 0 and valve not in opened_valves:
+        # If we open the valve, sum the current rate as well
         current_round_sum = all_valves[valve]['rate'] * minutes_left
-        open_valve_sums = [*map(lambda x: traverse_valves(x, all_valves,
-                                                          opened_valves + [valve], minutes_left - 2), related_valves)]
+        open_valve_sums = [*map(lambda x: current_round_sum + traverse_valves(
+            x, opened_valves + [valve], minutes_left - 2), related_valves)]
 
     # decide not to opened the valve
     not_open_valve_sums = [*map(lambda x: traverse_valves(
-        x, all_valves, opened_valves, minutes_left - 1), related_valves)]
+        x, opened_valves, minutes_left - 1), related_valves)]
 
     # return the better result
-    return current_round_sum + max(*[0], *open_valve_sums, *not_open_valve_sums)
+    result = max(*open_valve_sums, *not_open_valve_sums)
+    return result
 
 
 def part1():
     # build a map from the valves to the rate and related valves
-    valves = reduce(lambda result, row: result | extract_values_from_row(row),
-                    open("2022/input/day-16.input.txt"),
-                    {})
-    sum = traverse_valves('AA', valves, [], 30)
+    sum = traverse_valves('AA', [], 30)
     return sum
 
 
