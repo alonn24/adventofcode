675e5f599e8031db8f5f4c1b24c3b926428ecc08
Alony <alonn24@gmail.com>
2024-01-15 13:23:45 +0200

    add cache

--- 2022/python/day-16.py
+++ 2022/python/day-16.py
@@ -18,7 +18,13 @@ all_valves = reduce(lambda result, row: result | extract_values_from_row(row),
                     {})
 
 
-def traverse_valves(valve, opened_valves, minutes_left):
+def traverse_valves(valve, opened_valves, minutes_left, cache):
+    # search the cache as valve -> [...(opened_valves, minutes_left, result)]
+    cached = next(iter([v for v in cache[valve] if v[0] ==
+                  opened_valves and v[1] == minutes_left]), None)
+    if (cached is not None):
+        return cached[2]
+
     if (minutes_left <= 0):
         return 0
     # count the current valve by opened
@@ -28,22 +34,26 @@ def traverse_valves(valve, opened_valves, minutes_left):
     open_valve_sums = [0]
     if all_valves[valve]['rate'] > 0 and valve not in opened_valves:
         # If we open the valve, sum the current rate as well
-        current_round_sum = all_valves[valve]['rate'] * minutes_left
+        # If I opened the valve now, it will count only from the next round
+        current_round_sum = all_valves[valve]['rate'] * max(0, minutes_left-1)
         open_valve_sums = [*map(lambda x: current_round_sum + traverse_valves(
-            x, opened_valves + [valve], minutes_left - 2), related_valves)]
+            x, opened_valves + [valve], minutes_left - 2, cache), related_valves)]
 
     # decide not to opened the valve
     not_open_valve_sums = [*map(lambda x: traverse_valves(
-        x, opened_valves, minutes_left - 1), related_valves)]
+        x, opened_valves, minutes_left - 1, cache), related_valves)]
 
     # return the better result
     result = max(*open_valve_sums, *not_open_valve_sums)
+
+    # save in cache
+    cache[valve].append((opened_valves, minutes_left, result))
     return result
 
 
 def part1():
-    # build a map from the valves to the rate and related valves
-    sum = traverse_valves('AA', [], 30)
+    cache = dict([(i, []) for i in all_valves.keys()])
+    sum = traverse_valves('AA', [], 30, cache)
     return sum
 
 
