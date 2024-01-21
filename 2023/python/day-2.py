b6a21db05b47435fb538d9fb7939127b2fa44fc4
Alony <alonn24@gmail.com>
2024-01-21 14:21:51 +0200

    part 1

---
+++ 2023/python/day-2.py
@@ -0,0 +1,29 @@
+import re
+
+
+def break_input(row):
+    game = int(re.findall('Game (\d+):', row)[0])
+    blue = [int(x) for x in re.findall('(\d+) blue', row)]
+    red = [int(x) for x in re.findall('(\d+) red', row)]
+    green = [int(x) for x in re.findall('(\d+) green', row)]
+    return {'game': game, 'blue': blue, 'red': red, 'green': green}
+
+
+games = [break_input(row) for row in open("2023/input/day-2.input.txt", "r")]
+for game in games:
+    print(game)
+
+
+def is_game_possible(game, limits):
+    return max(game['blue'] + [0]) <= limits['blue'] and \
+        max(game['red'] + [0]) <= limits['red'] and \
+        max(game['green'] + [0]) <= limits['green']
+
+
+def part_1(limits):
+    return [game['game']
+            for game in games if is_game_possible(game, limits)]
+
+
+possible_games = part_1(limits={'blue': 14, 'red': 12, 'green': 13})
+print(f'part 1 - {sum(possible_games)}')  # 2348
