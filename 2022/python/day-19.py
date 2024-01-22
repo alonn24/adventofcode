6e2cdc96d9c76dcc9613215733d0cc886941d6cd
Alony <alonn24@gmail.com>
2024-01-22 16:03:07 +0200

    slow

--- 2022/python/day-19.py
+++ 2022/python/day-19.py
@@ -1,6 +1,7 @@
 import re
 from dataclasses import dataclass
-
+from collections import namedtuple
+from functools import cache
 input = open("2022/input/day-19.input.txt")
 
 
@@ -10,6 +11,9 @@ class Resources():
     clay: int = 0
     obsidian: int = 0
     geode: int = 0
+
+    def __hash__(self):
+        return hash((self.ore, self.clay, self.obsidian, self.geode))
 
     def has(self, other):
         return self.ore >= other.ore and self.clay >= other.clay and self.obsidian >= other.obsidian and self.geode >= other.geode
@@ -44,15 +48,23 @@ input = [*map(extract_values_from_row, open("2022/input/day-19.input.txt"))]
 
 
 def build_robots(available_resources: Resources, robots: Resources, blueprint: Blueprint):
+    # with the default one of not building a thing
+    states = [(available_resources, robots)]
+
+    # Add all possible robots build
     if available_resources.has(blueprint.geo_cost):
-        return (available_resources.subtract(blueprint.geo_cost), robots.add(Resources(geode=1)))
+        states.append((available_resources.subtract(
+            blueprint.geo_cost), robots.add(Resources(geode=1))))
     if available_resources.has(blueprint.obsidian_cost):
-        return (available_resources.subtract(blueprint.obsidian_cost), robots.add(Resources(obsidian=1)))
+        states.append((available_resources.subtract(
+            blueprint.obsidian_cost), robots.add(Resources(obsidian=1))))
     if available_resources.has(blueprint.clay_cost):
-        return (available_resources.subtract(blueprint.clay_cost), robots.add(Resources(clay=1)))
+        states.append((available_resources.subtract(
+            blueprint.clay_cost), robots.add(Resources(clay=1))))
     if available_resources.has(blueprint.ore_cost):
-        return (available_resources.subtract(blueprint.ore_cost), robots.add(Resources(ore=1)))
-    return (available_resources, robots)
+        states.append((available_resources.subtract(
+            blueprint.ore_cost), robots.add(Resources(ore=1))))
+    return states
 
 
 def collect_resources(available_resources: Resources, robots: Resources):
@@ -62,20 +74,23 @@ def collect_resources(available_resources: Resources, robots: Resources):
                      geode=available_resources.geode + robots.geode)
 
 
+state = namedtuple('state', ['available_resources', 'robots', 'minutes'])
+
+
 def get_quality_level(blueprint):
     print(blueprint)
+
+    @cache
+    def calculate_for_state(available_resources, robots, minutes):
+        print(available_resources, robots, minutes)
+        if minutes == 0:
+            return available_resources.geode
+        next_states = build_robots(available_resources, robots, blueprint)
+        return max([calculate_for_state(collect_resources(a, robots), b, minutes-1) for (a, b) in next_states])
+
     available_resources = Resources()
     robots = Resources(ore=1)
-    # 24 minutes
-    for _ in range(24):
-        (available_resources, new_robots) = build_robots(
-            available_resources, robots, blueprint)
-        available_resources = collect_resources(available_resources, robots)
-        # We later merge the new added robots so they wont collect resource this round
-        robots = new_robots
-        print(available_resources)
-        print('robots', robots, end='\n\n')
-    return available_resources.geode
+    return calculate_for_state(available_resources, robots, 24)
 
 
 def part_1():
