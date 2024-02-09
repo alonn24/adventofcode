b086c4be519c825d3bd60090c6bcfb136adecb31
Alony <alonn24@gmail.com>
2024-02-09 09:32:37 +0200

    one way

--- 2022/python/day_25.py
+++ 2022/python/day_25.py
@@ -1,15 +1,18 @@
 input = open('2022/input/day-25.input.txt').read().splitlines()
 
 
-def get_digit_value(d, i):
-    d = -1 if d == '-' else -2 if d == '=' else int(d)
-    return d * 5**i
-
-
 def get_entry_value(entry):
-    rev = reversed(entry) # starting from the end
+    def get_digit_value(d, i):
+        d = -1 if d == '-' else -2 if d == '=' else int(d)
+        return d * 5**i
+    rev = reversed(entry)  # starting from the end
     return sum([get_digit_value(d, i) for i, d in enumerate(rev)])
 
 
+def convert_to_decimal(snafu):
+    return snafu
+
+
 def part1():
-    return True
+    snafu = sum([get_entry_value(e) for e in input])
+    return convert_to_decimal(snafu)
--- 2022/python/day_25_test.py
+++ 2022/python/day_25_test.py
@@ -1,13 +1,4 @@
-from day_25 import get_digit_value, get_entry_value, part1
-
-
-def test_get_digit_value():
-    assert get_digit_value('1', 0) == 1
-    assert get_digit_value('1', 1) == 5
-    assert get_digit_value('2', 1) == 10
-    assert get_digit_value('1', 2) == 25
-    assert get_digit_value('2', 2) == 50
-    assert get_digit_value('1', 3) == 125
+from day_25 import get_entry_value, convert_to_decimal, part1
 
 
 def test_get_entry_value():
@@ -21,7 +12,10 @@ def test_get_entry_value():
     assert get_entry_value('10=') == 23
     assert get_entry_value('1-0') == 20
     assert get_entry_value('2=-01') == 1250-250-25+0+1 # 976
+    assert get_entry_value('1121-1110-1=0') == 314159265
 
+def test_convert_to_decimal():
+    assert convert_to_decimal(1) == 1
 
 def test_part1():
-    assert part1()
+    assert part1() == 4890
