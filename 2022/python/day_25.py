d4d271569514b3619c7f46a23323175c635f27f0
Alon <alonn24@users.noreply.github.com>
2024-02-10 09:21:47 +0200

    2022 day 25 (#12)
    * 2022 day 25
    
    * one way
    
    * part 1

---
+++ 2022/python/day_25.py
@@ -0,0 +1,24 @@
+input = open('2022/input/day-25.input.txt').read().splitlines()
+
+
+def get_entry_value(entry):
+    def get_digit_value(d, i):
+        d = -1 if d == '-' else -2 if d == '=' else int(d)
+        return d * 5**i
+    rev = reversed(entry)  # starting from the end
+    return sum([get_digit_value(d, i) for i, d in enumerate(rev)])
+
+
+def convert_to_snafu(d):
+    if d == 0:
+        return ''
+    current = '=-012'[(d+2) % 5];
+    return convert_to_snafu((d+2)//5) + current
+
+
+f = lambda s: f(s[:-1])*5 + '=-012'.find(s[-1])-2 if s else 0
+g = lambda d: g((d+2)//5) + '=-012'[(d+2)%5] if d else ''
+
+def part1():
+    d = sum([get_entry_value(e) for e in input])
+    return convert_to_snafu(d)
---
+++ 2022/python/day_25_test.py
@@ -0,0 +1,21 @@
+from day_25 import get_entry_value, convert_to_snafu, part1
+
+
+def test_get_entry_value():
+    assert get_entry_value('1') == 1
+    assert get_entry_value('10') == 5
+    assert get_entry_value('20') == 10
+    assert get_entry_value('100') == 25
+    assert get_entry_value('-') == -1
+    assert get_entry_value('=') == -2
+    assert get_entry_value('10-') == 24
+    assert get_entry_value('10=') == 23
+    assert get_entry_value('1-0') == 20
+    assert get_entry_value('2=-01') == 1250-250-25+0+1 # 976
+    assert get_entry_value('1121-1110-1=0') == 314159265
+
+def test_convert_to_snafu():
+    assert convert_to_snafu(1) == '1'
+
+def test_part1():
+    assert part1() == '2=001=-2=--0212-22-2'
