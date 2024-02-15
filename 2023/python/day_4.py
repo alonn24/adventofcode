1c1495846aaa97c35b4a5ec052cb878848f87d7a
Alony <alonn24@gmail.com>
2024-02-15 21:45:53 +0200

    part 2

--- 2023/python/day_4.py
+++ 2023/python/day_4.py
@@ -10,8 +10,19 @@ def parse_row(row):
 
 
 def part1(input):
-    print('')
     cards = [parse_row(row) for row in input]
-    intersected = [np.intersect1d(a, b).size for [a, b] in cards]
-    positive_intersections = [x for x in intersected if x > 0]
+    intersected = [np.intersect1d(a, b) for [a, b] in cards]
+    positive_intersections = [x.size for x in intersected if x.size > 0]
     return reduce(lambda res, y: res + 2**(y-1), positive_intersections, 0)
+
+
+def part2(input):
+    cards = [parse_row(row) for row in input]
+    intersected_sizes = [np.intersect1d(a, b).size for [a, b] in cards]
+
+    res = np.array([1 for _ in range(len(cards))])
+    for i, size in enumerate(intersected_sizes):
+        if (size > 0):
+            number_of_cards = res[i]
+            res[i+1:i+1+size] += number_of_cards
+    return np.sum(res, dtype=np.int64)
--- 2023/python/day_4_test.py
+++ 2023/python/day_4_test.py
@@ -1,8 +1,14 @@
 import pytest
-from day_4 import part1
+from day_4 import part1, part2
 
 def test_part1_test_case():
 	assert part1(open('2023/input/day-4.test.txt').read().splitlines()) == 13
 
 def test_part1_real_case():
 	assert part1(open('2023/input/day-4.input.txt').read().splitlines()) == 21088
+
+def test_part2_test_case():
+	assert part2(open('2023/input/day-4.test.txt').read().splitlines()) == 30
+
+def test_part2_real_case():
+	assert part2(open('2023/input/day-4.input.txt').read().splitlines()) == 6874754
