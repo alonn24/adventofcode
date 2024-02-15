be1d5ffef48e5f3bff200c73cc5d36cd76e09648
Alony <alonn24@gmail.com>
2024-02-15 14:15:22 +0200

    part 2

--- 2023/python/day_3.py
+++ 2023/python/day_3.py
@@ -1,15 +1,19 @@
 import numpy as np
+from functools import reduce
 
 
-def collect(data, i, j):
-    row = data[i]
-    start = j
-    while start > 0 and row[start - 1].isdigit():
-        start -= 1
-    end = j
-    while end < len(row) - 1 and row[end + 1].isdigit():
-        end += 1
-    return f'{i}:{start}:{end+1}', int(''.join(row[start:end+1]))
+def collection_numbers(data, ixs):
+    ns = {}
+    for i, j in ixs:
+        row = data[i]
+        start = j
+        end = j
+        while start > 0 and row[start - 1].isdigit():
+            start -= 1
+        while end < len(row) - 1 and row[end + 1].isdigit():
+            end += 1
+        ns[f'{i}:{start}:{end+1}'] = int(''.join(row[start:end+1]))
+    return ns
 
 
 def part1(input):
@@ -32,8 +36,27 @@ def part1(input):
         digits & np.pad(symbols[:-1, 1:], [(1, 0), (0, 1)])
 
     ixs = [*zip(*intersections.nonzero())]
-    ns = {}
-    for i, j in ixs:
-        key, res = collect(data, i,  j)
-        ns[key] = res
+    ns = collection_numbers(data, ixs)
     return sum(n for n in ns.values())
+
+
+GEAR = '*'
+
+d = [[1, 0], [0, 1], [-1, 0], [0, -1], [1, 1], [-1, -1], [1, -1], [-1, 1]]
+
+
+def is_in_bounds(data, i, j):
+    return 0 <= i < len(data) and 0 <= j < len(data[0])
+
+
+def part2(input):
+    data = np.array([[*row] for row in input])
+    ixs = [*zip(*(data == '*').nonzero())]
+    res = 0
+    for i, j in ixs:
+        ixs = [[i + d[0], j + d[1]]
+               for d in d if is_in_bounds(data, i + d[0], j + d[1]) and data[i + d[0], j + d[1]].isdigit()]
+        ns = collection_numbers(data, ixs)
+        if (len(ns) > 1):
+            res += reduce(lambda acc, n: acc * n, ns.values(), 1)
+    return res
--- 2023/python/day_3_test.py
+++ 2023/python/day_3_test.py
@@ -1,6 +1,4 @@
-import pytest
-from day_3 import part1
-
+from day_3 import part1, part2
 
 def test_part1_test_case():
 	assert part1(open('2023/input/day-3.input.txt').read().splitlines()) == 4361
@@ -8,3 +6,9 @@ def test_part1_test_case():
 def test_part1_real_case():
 	# 326410 too low
 	assert part1(open('2023/input/day-3-real.input.txt').read().splitlines()) == 539433
+
+def test_part2_test_case():
+	assert part2(open('2023/input/day-3.input.txt').read().splitlines()) == 467835
+
+def test_part2_real_case():
+	assert part2(open('2023/input/day-3-real.input.txt').read().splitlines()) == 75847567
