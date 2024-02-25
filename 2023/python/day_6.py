f2bf3f45b9675ea0dde90160b3b28648cc9570ba
Alony <alonn24@gmail.com>
2024-02-25 20:23:14 +0200

    2023 day 6 part 1

---
+++ 2023/python/day_6.py
@@ -0,0 +1,18 @@
+import re
+import numpy as np
+
+
+def beat_record_ways(time, distance):
+    result = np.arange(0, time + 1)
+    result = result * (time-result)
+    return len(result[result > distance])
+
+
+def part1(input):
+    times = [*map(int, re.findall(r'\d+', input[0]))]
+    distances = [*map(int, re.findall(r'\d+', input[1]))]
+
+    result = 1
+    for i in range(len(times)):
+        result *= beat_record_ways(times[i], distances[i])
+    return result
