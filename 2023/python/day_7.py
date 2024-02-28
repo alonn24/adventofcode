1ac858a381666d6c7d08110c204875c66446ac78
Alony <alonn24@gmail.com>
2024-02-28 17:03:05 +0200

    comments

--- 2023/python/day_7.py
+++ 2023/python/day_7.py
@@ -1,5 +1,10 @@
 import numpy as np
 
+"""
+--- Day 7: Camel Cards ---
+Sort the hands of cards and calculate the score based on the rand and the bid
+"""
+
 
 def part1(_input):
     def get_sorted_hands(data):
@@ -23,8 +28,9 @@ def part1(_input):
                 2 if is_one_pair else \
                 1
 
-        hand_types = np.array([get_hand_type(hand) for hand in data[:, 0]])
+        hand_types = np.vectorize(get_hand_type)(data[:, 0])
         sorted_hands = np.array([]).reshape(-1, 2)
+        # Internally sort each hand type and stack back
         for hand_type in range(1, 8):
             hands_of_type = data[hand_types == hand_type]
             sorted_hands_of_type = sorted(hands_of_type, key=lambda x: (order.index(x[0][0]), order.index(
@@ -37,6 +43,12 @@ def part1(_input):
     sorted_data = get_sorted_hands(data)
     factors = np.arange(1, len(sorted_data) + 1)
     return np.sum(sorted_data[:, 1].astype(int) * factors)
+
+
+"""
+--- Day 7: Camel Cards ---
+Now use J and a Jocker that can act as any card
+"""
 
 
 def part2(_input):
@@ -54,7 +66,7 @@ def part2(_input):
             is_three_of_a_kind = sorted_counts[0] == 3
             is_two_pairs = sorted_counts[0] == 2 and sorted_counts[1] == 2
             is_one_pair = sorted_counts[0] == 2
-            score =  7 if is_five_of_a_kind else \
+            score = 7 if is_five_of_a_kind else \
                 7 if is_four_of_a_kind and j_count == 1 else \
                 6 if is_four_of_a_kind else \
                 5 if is_full_house else \
@@ -75,12 +87,15 @@ def part2(_input):
                 1
             return score
 
-        hand_types = np.array([get_hand_type(hand) for hand in data[:, 0]])
+        hand_types = np.vectorize(get_hand_type)(data[:, 0])
+
+        # Internally sort each hand type and stack back
         sorted_hands = np.array([]).reshape(-1, 2)
         for hand_type in range(1, 8):
             hands_of_type = data[hand_types == hand_type]
             sorted_hands_of_type = sorted(hands_of_type, key=lambda x: (order.index(x[0][0]), order.index(
                 x[0][1]), order.index(x[0][2]), order.index(x[0][3]), order.index(x[0][4])))
+            # Failed to add an empty array because of missing shape, so we check
             if len(sorted_hands_of_type):
                 sorted_hands = np.vstack([sorted_hands, sorted_hands_of_type])
         return sorted_hands
