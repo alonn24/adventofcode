cf29df108c637f1f998c979fc93a1f6f58a4704c
Alony <alonn24@gmail.com>
2024-02-28 16:49:43 +0200

    part 2 wrong

--- 2023/python/day_7.py
+++ 2023/python/day_7.py
@@ -1,42 +1,84 @@
 import numpy as np
 
 
-def get_sorted_hands(data):
-    order = "23456789TJQKA"
-
-    def get_hand_type(hand):
-        cards_count = np.bincount([order.index(card)
-                                  for card in hand], minlength=len(order))
-        sorted_counts = np.sort(cards_count)[::-1]
-        if sorted_counts[0] == 5:
-            return 7
-        elif sorted_counts[0] == 4:
-            return 6
-        elif sorted_counts[0] == 3 and sorted_counts[1] == 2:
-            return 5
-        elif sorted_counts[0] == 3:
-            return 4
-        elif sorted_counts[0] == 2 and sorted_counts[1] == 2:
-            return 3
-        elif sorted_counts[0] == 2:
-            return 2
-        else:
-            return 1
-
-    hand_types = np.array([get_hand_type(hand) for hand in data[:, 0]])
-    sorted_hands = np.array([]).reshape(-1, 2)
-    for hand_type in range(1, 8):
-        hands_of_type = data[hand_types == hand_type]
-        print(hand_type, hands_of_type)
-        sorted_hands_of_type = sorted(hands_of_type, key=lambda x: (order.index(x[0][0]), order.index(
-            x[0][1]), order.index(x[0][2]), order.index(x[0][3]), order.index(x[0][4])))
-        if len(sorted_hands_of_type):
-            sorted_hands = np.vstack([sorted_hands, sorted_hands_of_type])
-    return sorted_hands
-
-
 def part1(_input):
-    print('')
+    def get_sorted_hands(data):
+        order = "23456789TJQKA"
+
+        def get_hand_type(hand):
+            cards_count = np.bincount([order.index(card)
+                                      for card in hand], minlength=len(order))
+            sorted_counts = np.sort(cards_count)[::-1]
+            is_five_of_a_kind = sorted_counts[0] == 5
+            is_four_of_a_kind = sorted_counts[0] == 4
+            is_full_house = sorted_counts[0] == 3 and sorted_counts[1] == 2
+            is_three_of_a_kind = sorted_counts[0] == 3
+            is_two_pairs = sorted_counts[0] == 2 and sorted_counts[1] == 2
+            is_one_pair = sorted_counts[0] == 2
+            return 7 if is_five_of_a_kind else \
+                6 if is_four_of_a_kind else \
+                5 if is_full_house else \
+                4 if is_three_of_a_kind else \
+                3 if is_two_pairs else \
+                2 if is_one_pair else \
+                1
+
+        hand_types = np.array([get_hand_type(hand) for hand in data[:, 0]])
+        sorted_hands = np.array([]).reshape(-1, 2)
+        for hand_type in range(1, 8):
+            hands_of_type = data[hand_types == hand_type]
+            sorted_hands_of_type = sorted(hands_of_type, key=lambda x: (order.index(x[0][0]), order.index(
+                x[0][1]), order.index(x[0][2]), order.index(x[0][3]), order.index(x[0][4])))
+            if len(sorted_hands_of_type):
+                sorted_hands = np.vstack([sorted_hands, sorted_hands_of_type])
+        return sorted_hands
+
+    data = np.array([x.split(' ') for x in _input])
+    sorted_data = get_sorted_hands(data)
+    factors = np.arange(1, len(sorted_data) + 1)
+    return np.sum(sorted_data[:, 1].astype(int) * factors)
+
+
+def part2(_input):
+    def get_sorted_hands(data):
+        order = "J23456789TQKA"
+
+        def get_hand_type(hand):
+            cards_count = np.bincount([order.index(card)
+                                      for card in hand], minlength=len(order))
+            sorted_counts = np.sort(cards_count[1:])[::-1]
+            j_count = cards_count[0]
+            is_five_of_a_kind = sorted_counts[0] == 5
+            is_four_of_a_kind = sorted_counts[0] == 4
+            is_full_house = sorted_counts[0] == 3 and sorted_counts[1] == 2
+            is_three_of_a_kind = sorted_counts[0] == 3
+            is_two_pairs = sorted_counts[0] == 2 and sorted_counts[1] == 2
+            is_one_pair = sorted_counts[0] == 2
+            return 7 if is_five_of_a_kind else \
+                7 if is_four_of_a_kind and j_count == 1 else \
+                6 if is_four_of_a_kind else \
+                5 if is_full_house else \
+                7 if is_three_of_a_kind and j_count == 2 else \
+                6 if is_three_of_a_kind and j_count == 1 else \
+                4 if is_three_of_a_kind else \
+                5 if is_two_pairs and j_count == 1 else \
+                3 if is_two_pairs else \
+                7 if is_one_pair and j_count == 3 else \
+                6 if is_one_pair and j_count == 2 else \
+                4 if is_one_pair and j_count == 1 else \
+                2 if is_one_pair else \
+                1
+
+        hand_types = np.array([get_hand_type(hand) for hand in data[:, 0]])
+        sorted_hands = np.array([]).reshape(-1, 2)
+        for hand_type in range(1, 8):
+            hands_of_type = data[hand_types == hand_type]
+            sorted_hands_of_type = sorted(hands_of_type, key=lambda x: (order.index(x[0][0]), order.index(
+                x[0][1]), order.index(x[0][2]), order.index(x[0][3]), order.index(x[0][4])))
+            if len(sorted_hands_of_type):
+                sorted_hands = np.vstack([sorted_hands, sorted_hands_of_type])
+        return sorted_hands
+
     data = np.array([x.split(' ') for x in _input])
     sorted_data = get_sorted_hands(data)
     factors = np.arange(1, len(sorted_data) + 1)
--- 2023/python/day_7_test.py
+++ 2023/python/day_7_test.py
@@ -1,16 +1,4 @@
-import pytest
-import numpy as np
-from day_7 import part1, get_sorted_hands
-
-
-@pytest.mark.parametrize('hands, expected', [
-    ([['AAAA2', 2], ['AAA23', 1]], [['AAA23', 1], ['AAAA2', 2]]),
-    ([['AAAA3', 2], ['AAAA2', 1]], [['AAAA2', 1], ['AAAA3', 2]])
-])
-def test_get_sorted_hands(hands, expected):
-    data = np.array(hands)
-    result = get_sorted_hands(data)
-    assert np.array_equal(result, expected)
+from day_7 import part1, part2
 
 
 def test_part_1_test_case():
@@ -20,3 +8,14 @@ def test_part_1_test_case():
 def test_part_1_real_case():
     assert part1(
         open('2023/input/day-7.input.txt').read().splitlines()) == 250347426
+
+
+def test_part_2_test_case():
+    assert part2(open('2023/input/day-7.test.txt').read().splitlines()) == 5905
+
+# 251598109 too high
+
+
+def test_part_2_real_case():
+    assert part2(
+        open('2023/input/day-7.input.txt').read().splitlines()) == 251598109
