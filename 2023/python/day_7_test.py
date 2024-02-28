5b7a61c199c5cdb5852289a60af3ed3d2ba14f55
Alon <alonn24@users.noreply.github.com>
2024-02-28 16:56:09 +0200

    2023 day 7 (#19)
    * part 1
    
    * part 2 wrong
    
    * part 2

---
+++ 2023/python/day_7_test.py
@@ -0,0 +1,21 @@
+from day_7 import part1, part2
+
+
+def test_part_1_test_case():
+    assert part1(open('2023/input/day-7.test.txt').read().splitlines()) == 6440
+
+
+def test_part_1_real_case():
+    assert part1(
+        open('2023/input/day-7.input.txt').read().splitlines()) == 250347426
+
+
+def test_part_2_test_case():
+    assert part2(open('2023/input/day-7.test.txt').read().splitlines()) == 5905
+
+# 251598109 too high
+# 251181217 too low
+
+def test_part_2_real_case():
+    assert part2(
+        open('2023/input/day-7.input.txt').read().splitlines()) == 251224870
