6011aa8906bac5096d5a36963db5b532b5d7dbd7
Alony <alonn24@gmail.com>
2024-03-10 16:17:16 +0200

    day 13 part 2

--- 2023/python/day_13_test.py
+++ 2023/python/day_13_test.py
@@ -1,4 +1,4 @@
-from day_13 import part1
+from day_13 import part1, part2
 
 
 def test_day13_part1_test_case():
@@ -11,3 +11,15 @@ def test_day13_part1_real_case():
     with open('2023/input/day-13.input.txt', 'r') as f:
         data = f.read()
         assert part1(data) == 30705
+
+
+def test_day13_part2_test_case():
+    with open('2023/input/day-13.test.txt', 'r') as f:
+        data = f.read()
+        assert part2(data) == 400
+
+
+def test_day13_part2_real_case():
+    with open('2023/input/day-13.input.txt', 'r') as f:
+        data = f.read()
+        assert part2(data) == 44615
