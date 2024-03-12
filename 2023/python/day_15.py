599985a0cea0bc2c188c93bf0bb23a46aa150084
Alony <alonn24@gmail.com>
2024-03-12 08:38:03 +0200

    Part 2

--- 2023/python/day_15.py
+++ 2023/python/day_15.py
@@ -1,4 +1,5 @@
 import numpy as np
+from typing import Any
 
 
 def decode_part(part: str):
@@ -17,3 +18,51 @@ def part1(case: str):
     parts = np.array(case.split(','))
     decoded = [decode_part(part) for part in parts]
     return sum(decoded)
+
+
+def part2(case: str):
+    """
+    Part 2 - organize the lens in boxes
+    """
+    parts = np.array(case.split(','))
+    lens = np.unique(np.array([part.split('=')[0] for part in parts if '=' in part]))
+    index_map = {item: index for index, item in enumerate(lens)}
+
+    # Box index are the rows and the lens are the columns
+    # The column index is the parts index
+    # We save the round count so we can sort later on
+    # boxes = (box, lens, focal length)
+    boxes = np.zeros((256, len(lens), 2), dtype=np.int64)
+    for i, part in enumerate(parts):
+        if '-' in part:
+            # Turn off the lens
+            lens = part.split('-')[0]
+            if not lens in index_map:
+                continue
+            box = decode_part(lens)
+            boxes[box, index_map[lens], 0] = 0
+        if '=' in part:
+            # Turn on the lens with the round number
+            lens, focal = part.split('=')
+            if not lens in index_map:
+                continue
+            box = decode_part(lens)
+            # Set order only if not exists
+            if boxes[box, index_map[lens], 0] == 0:
+                boxes[box, index_map[lens], 0] = i + 1
+            boxes[box, index_map[lens], 1] = focal
+
+    # Calculate the score for each box
+    scores = np.array([get_box_score(box, i) for i, box in enumerate(boxes)])
+    return np.sum(scores)
+
+
+def get_box_score(box: np.ndarray[int, Any], box_idx: int):
+    # remove zeros
+    box = box[box[:, 0] != 0]
+    # sort by first column - order value
+    box = box[np.argsort(box[:, 0])]
+
+    # Each lens score is (box index * lens index * focal length)
+    box_scores = [(box_idx+1) * (i + 1) * box[i, 1] for i in range(len(box))]
+    return sum(box_scores)
--- 2023/python/day_15_test.py
+++ 2023/python/day_15_test.py
@@ -1,11 +1,21 @@
-from day_15 import part1
+from day_15 import part1, part2
 
 
 def test_day15_part1_test_case():
     assert part1('rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7') == 1320
 
 
-def test_day15_real_case():
+def test_day15_part1_real_case():
     with open('2023/input/day-15.input.txt', 'r') as file:
         data = file.read()
         assert part1(data) == 508498
+
+
+def test_day15_part2_test_case():
+    assert part2('rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7') == 145
+
+
+def test_day15_part2_real_case():
+    with open('2023/input/day-15.input.txt', 'r') as file:
+        data = file.read()
+        assert part2(data) == 279116
