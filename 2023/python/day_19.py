f8495619e76cf46dae9d6d6fa43d0d5ba18474a1
Alony <alonn24@gmail.com>
2024-03-30 17:56:32 +0300

    Day 19 part 2

--- 2023/python/day_19.py
+++ 2023/python/day_19.py
@@ -28,7 +28,7 @@ def parse_case(case: str):
 # ['a', '<', '3', 'b']
 Rule = tuple[str, str, str, str]
 # {'in': ([['a', '<', '3', 'b']...], 'out')}
-Workflows = dict[str, tuple[np.ndarray[int, Any], str]]
+Workflows = dict[str, tuple[np.ndarray[str, Any], str]]
 
 
 def check_rule(rating: np.ndarray[int, Any], rule: Rule) -> bool:
@@ -50,7 +50,6 @@ def check_rating(rating: np.ndarray[int, Any], workflows: Workflows, node: str =
 
 
 def part1(case: str):
-    print('')
     """
     Day 19: Aplenty
     part 1 - Run all ratings in front of the workflows and return the sum of ratings that pass
@@ -58,3 +57,67 @@ def part1(case: str):
     workflows, ratings = parse_case(case)
     accepted_ratings = [r for r in ratings if check_rating(r, workflows)]
     return np.sum(accepted_ratings)
+
+
+MIN = 1
+MAX = 4000
+
+not_convert = {
+    '>': '<=',
+    '<': '>=',
+}
+
+
+def negate_rules(rule: list[Rule]) -> list[Rule]:
+    return [(r[0], not_convert[r[1]], r[2], r[3]) for r in rule]
+
+
+def get_combinations(path: list[Rule]) -> int:
+    # Inclusive ranges
+    ranges: list[tuple[str, int, int]] = [(x, MIN, MAX) for x in rating_indices]
+    for node in path:
+        idx = rating_indices.index(node[0])
+        equal_addition = 0 if node[1] == '<=' or node[1] == '>=' else 1
+        if node[1] == '<' or node[1] == '<=':
+            # Less than - reduce the max value
+            new_top = min(int(node[2]) - equal_addition, ranges[idx][2])
+            ranges[idx] = (ranges[idx][0], ranges[idx][1], new_top)
+        else:
+            # greater than - increase the min value
+            new_bottom = max(int(node[2]) + equal_addition, ranges[idx][1])
+            ranges[idx] = (ranges[idx][0], new_bottom, ranges[idx][2])
+    # Get the range length - inclusive
+    result = [r[2] - r[1] + 1 for r in ranges]
+    # Return the product of all ranges which is the number of combinations
+    return result[0]*result[1]*result[2]*result[3]
+
+
+def part2(case: str):
+    """
+    Part 2 - Calculate the number of combinations that will pass the workflows
+    """
+    workflows, _ = parse_case(case)
+    win_paths: list[list[Any]] = []
+    q: list[tuple[list[Any], str]] = [([], START_NODE)]
+    while q:
+        path, node = q.pop()
+        # End a path
+        if node == ACCEPT:
+            win_paths.append(path)
+            continue
+        # Reject a path
+        if node == REJECT:
+            continue
+
+        # Continue a path
+        workflow = workflows[node]
+        rules = workflow[0]
+        for i, rule in enumerate(rules):
+            # Negate previous rules
+            previous_rules = rules[0:i]
+            negated_previous_rules = [x[0:3] for x in negate_rules(list(previous_rules))]
+            # Append the rule and continue with the accept node
+            q.append((path + negated_previous_rules + [tuple(rule[0:3])], rule[3]))
+        # Continue with the else node
+        q.append((path + [x[0:3] for x in negate_rules(list(rules))], workflow[1]))
+    return sum([get_combinations(path) for path in win_paths])
--- 2023/python/day_19_test.py
+++ 2023/python/day_19_test.py
@@ -1,4 +1,4 @@
-from day_19 import part1
+from day_19 import part1, part2
 
 
 def test_day19_part1_testcase():
@@ -11,3 +11,15 @@ def test_day19_part1_realcase():
     with open('2023/input/day-19.input.txt', 'r') as file:
         case = file.read()
         assert part1(case) == 391132
+
+
+def test_day19_part2_testcase():
+    with open('2023/input/day-19.test.txt', 'r') as file:
+        case = file.read()
+        assert part2(case) == 167409079868000
+
+
+def test_day19_part2_realcase():
+    with open('2023/input/day-19.input.txt', 'r') as file:
+        case = file.read()
+        assert part2(case) == 128163929109524
