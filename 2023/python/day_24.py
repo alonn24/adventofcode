a31dcc0fe7739fedd38f1b035563d36592c79173
Alony <alonn24@gmail.com>
2024-04-17 19:04:32 +0300

    Part 2

--- 2023/python/day_24.py
+++ 2023/python/day_24.py
@@ -40,3 +40,89 @@ def part1(case: str, min: int, max: int):
         if min <= xpos <= max and min <= ypos <= max:
             count += 1
     return count
+
+
+def part2(case: str):
+    hailstones = np.array(re.findall(r'-?\d+', case), dtype=np.int64).reshape(-1, 6)
+    n = len(hailstones)
+
+    pts = hailstones[:, :3].tolist()
+    vels = hailstones[:, 3:].tolist()
+    p1, v1 = pts[0], vels[0]
+    for i in range(1, n):
+        if indep(v1, vels[i]):
+            p2, v2 = pts[i], vels[i]
+            break
+    for j in range(i+1, n):
+        if indep(v1, vels[j]) and indep(v2, vels[j]):
+            p3, v3 = pts[j], vels[j]
+            break
+
+    rock, S = find_rock(p1, v1, p2, v2, p3, v3)
+    return sum(rock) / S
+
+
+def find_rock(p1: list[int], v1: list[int], p2: list[int], v2: list[int], p3: list[int], v3: list[int]):
+    a, A = find_plane(p1, v1, p2, v2)
+    b, B = find_plane(p1, v1, p3, v3)
+    c, C = find_plane(p2, v2, p3, v3)
+
+    w = lin(A, cross(b, c), B, cross(c, a), C, cross(a, b))
+    wx, wy, wz = w
+    t = dot(a, cross(b, c))
+    # given that w is integer, so force it here to avoid carrying through
+    # imprecision
+    # rest of the computation is integer except the final division
+    w = [round(wx / t), round(wy / t), round(wz / t)]
+
+    w1 = sub(v1, w)
+    w2 = sub(v2, w)
+    ww = cross(w1, w2)
+
+    E = dot(ww, cross(p2, w2))
+    F = dot(ww, cross(p1, w1))
+    G = dot(p1, ww)
+    S = dot(ww, ww)
+
+    rock = lin(E, w1, -F, w2, G, ww)
+    return (rock, S)
+
+
+def find_plane(p1: list[int], v1: list[int], p2: list[int], v2: list[int]):
+    p12 = sub(p1, p2)
+    v12 = sub(v1, v2)
+    vv = cross(v1, v2)
+    return (cross(p12, v12), dot(p12, vv))
+
+
+def cross(a: list[int], b: list[int]):
+    ax, ay, az = a
+    ba, by, bz = b
+    return [ay*bz - az*by, az*ba - ax*bz, ax*by - ay*ba]
+
+
+def dot(a: list[int], b: list[int]):
+    ax, ay, az = a
+    ba, by, bz = b
+    return ax*ba + ay*by + az*bz
+
+
+def sub(a: list[int], b: list[int]):
+    ax, ay, az = a
+    ba, by, bz = b
+    return [ax-ba, ay-by, az-bz]
+
+
+def lin(r: int, a: list[int], s: int, b: list[int], t: int, c: list[int]):
+    ax, ay, az = a
+    bx, by, bz = b
+    cx, cy, cz = c
+    return [
+        r*ax + s*bx + t*cx,
+        r*ay + s*by + t*cy,
+        r*az + s*bz + t*cz
+    ]
+
+
+def indep(a: list[int], b: list[int]):
+    return any(v != 0 for v in cross(a, b))
--- 2023/python/day_24_test.py
+++ 2023/python/day_24_test.py
@@ -1,4 +1,4 @@
-from day_24 import part1
+from day_24 import part1, part2
 testcase = '''
 19, 13, 30 @ -2,  1, -2
 18, 19, 22 @ -1, -1, -2
@@ -15,3 +15,12 @@ def test_day24_part1_testcase():
 def test_day24_part1_realcase():
     with open('2023/input/day-24.input.txt') as f:
         assert part1(f.read(), 200000000000000, 400000000000000) == 13892
+
+
+def test_day24_part2_testcase():
+    assert part2(testcase) == 47
+
+
+def test_day24_part2_realcase():
+    with open('2023/input/day-24.input.txt') as f:
+        assert part2(f.read()) == 843888100572888
