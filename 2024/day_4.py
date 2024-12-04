9b398bafdf732961e168498782ec819f86a3089f
Alony <alonn24@gmail.com>
2024-12-04 17:16:15 +0200

    Day 4 part 2

--- 2024/day_4.py
+++ 2024/day_4.py
@@ -1,8 +1,9 @@
 import numpy as np
 from typing import Any
 
-all_directions = [(0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+
 word = ['X', 'M', 'A', 'S']
+all_directions = [(0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
 
 
 def check_word(lines: np.ndarray[Any, Any], pos: tuple[int, int], dir: tuple[int, int]):
@@ -25,6 +26,50 @@ def part1(testcase: str):
         np.empty((3, lines.shape[1]), dtype=str)
     ])
 
-    start_positions = np.argwhere(lines == word[0])
+    # Start from X and search for MAS in all directions
+    start_positions = np.argwhere(lines == 'X')
+
     xmas = [(pos, dir) for pos in start_positions for dir in all_directions if check_word(lines, pos, dir)]
+    return len(xmas)
+
+
+letters = ['M', 'S']
+
+
+def check_word_x(lines: np.ndarray[Any, Any], pos: tuple[int, int]):
+    res = (
+        ((
+            # top left to bottom right
+            (lines[pos[0]-1, pos[1]-1] == letters[0] and
+             lines[pos[0]+1, pos[1]+1] == letters[1]))
+            or (lines[pos[0]-1, pos[1]-1] == letters[1] and
+                lines[pos[0]+1, pos[1]+1] == letters[0]))
+        and ((
+            # top right to bottom left
+            (lines[pos[0]-1, pos[1]+1] == letters[0] and
+             lines[pos[0]+1, pos[1]-1] == letters[1]))
+             or (lines[pos[0]-1, pos[1]+1] == letters[1] and
+                 lines[pos[0]+1, pos[1]-1] == letters[0]))
+    )
+    return res
+
+
+def part2(testcase: str):
+    lines = np.array([[*x] for x in testcase.splitlines()])
+    # Stack 3 columns on each side
+    lines = np.hstack([
+        np.empty((lines.shape[0], 1), dtype=str),
+        lines,
+        np.empty((lines.shape[0], 1), dtype=str)
+    ])
+    # Stack 3 rows on each side
+    lines = np.vstack([
+        np.empty((1, lines.shape[1]), dtype=str),
+        lines,
+        np.empty((1, lines.shape[1]), dtype=str)
+    ])
+
+    # Start from A and search for MAS in diagonals
+    start_positions = np.argwhere(lines == 'A')
+    xmas = [pos for pos in start_positions if check_word_x(lines, pos)]
     return len(xmas)
--- 2024/day_4_test.py
+++ 2024/day_4_test.py
@@ -1,4 +1,4 @@
-from day_4 import part1
+from day_4 import part1, part2
 
 
 def test_day4_part1_test_case():
@@ -19,3 +19,23 @@ def test_day4_part1_real_case():
     with open('day_4.input.txt', 'r') as f:
         testcase = f.read()
     assert part1(testcase) == 2562
+
+
+def test_day4_part2_test_case():
+    testcase = '''MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX'''
+    assert part2(testcase) == 9
+
+
+def test_day4_part2_real_case():
+    with open('day_4.input.txt', 'r') as f:
+        testcase = f.read()
+    assert part2(testcase) == 1902
