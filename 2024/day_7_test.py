fe8ad16b00553ce5fba0018966b8dbc73270e4db
Alony <alonn24@gmail.com>
2024-12-19 08:06:36 +0200

    Day 7 part 2

--- 2024/day_7_test.py
+++ 2024/day_7_test.py
@@ -1,4 +1,4 @@
-from day_7 import part1
+from day_7 import part1, part2
 
 
 def test_day7_part1_test_case():
@@ -16,7 +16,26 @@ def test_day7_part1_test_case():
 # 532539761362 too low
 
 
-def test_day7_real_case():
+def test_day7_part1_real_case():
     with open('day_7.input.txt', 'r') as f:
         testcase = f.read()
     assert part1(testcase) == 538191549061
+
+
+def test_day7_part2_test_case():
+    testcase = '''190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20'''
+    assert part2(testcase) == 11387
+
+
+def test_day7_part2_real_case():
+    with open('day_7.input.txt', 'r') as f:
+        testcase = f.read()
+    assert part2(testcase) == 34612812972206
